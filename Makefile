04877cdacc905dcc13813c0bc2e860be81d4777d
khuxkm fbexl <khuxkm@ttm.sh>
2018-12-15 01:02:46 -0500

    Add games.json file

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+all: games.zip games.json
+
 games.zip: $(wildcard games/*)
 	cd games && zip -9 -r ../games.zip .
+
+games.json: $(wildcard games/*)
+	python games.json.py
---
+++ games.json.py
@@ -0,0 +1,14 @@
+import os, re, json
+
+OUT = dict()
+games = []
+
+for file in os.listdir("games"):
+	if file.endswith(".swf"):
+		games.append(re.sub("([a-z0-9])([A-Z0-9])",r"\g<0> \g<1>",file[:-4]))
+
+OUT["games"] = games
+OUT["count"] = len(games)
+
+with open("games.json","w") as f:
+	json.dump(OUT,f)
