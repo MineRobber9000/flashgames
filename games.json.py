487515b2956ca82029ee918f544a6edf0e3f9aa2
khuxkm fbexl <khuxkm@ttm.sh>
2018-12-15 01:04:04 -0500

    Fix games.json

--- games.json.py
+++ games.json.py
@@ -5,7 +5,7 @@ games = []
 
 for file in os.listdir("games"):
 	if file.endswith(".swf"):
-		games.append(re.sub("([a-z0-9])([A-Z0-9])",r"\g<0> \g<1>",file[:-4]))
+		games.append(re.sub("([a-z0-9])([A-Z0-9])",r"\g<1> \g<2>",file[:-4]))
 
 OUT["games"] = games
 OUT["count"] = len(games)
